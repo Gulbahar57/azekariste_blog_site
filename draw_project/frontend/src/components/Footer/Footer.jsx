89e23d1cc5c341e4ada891f2d77e641a45a56826
Gulbahar57 <158152207+Gulbahar57@users.noreply.github.com>
2024-09-09 15:36:14 +0200

    add some files for pages, add links in the footer.jsx and adjusting footer.css

--- draw_project/frontend/src/components/Footer/Footer.jsx
+++ draw_project/frontend/src/components/Footer/Footer.jsx
@@ -1,26 +1,27 @@
-import React from "react";
-import './Footer.css';
-import instagramlogo from '../../assets/img/instagramlogo.svg'
-import tiktoklogo from '../../assets/img/tiktoklogo.svg'
+    import React from "react";
+    import './Footer.css';
+    import instagramlogo from '../../assets/img/instagramlogo.svg'
+    import tiktoklogo from '../../assets/img/tiktoklogo.svg'
+    import { Link } from "react-router-dom";
 
-function Footer() {
-    return(
-        <footer>
-            <div className="purple-band">
-                <div className="social-links">
-                    <img src={instagramlogo} alt="instagramlogo"/>
-                    <img src={tiktoklogo} alt="tiktoklogo"/>
+    function Footer() {
+        return(
+            <footer>
+                <div className="purple-band">
+                    <div className="social-links">
+                        <img src={instagramlogo} alt="instagramlogo"/>
+                        <img src={tiktoklogo} alt="tiktoklogo"/>
+                    </div>
+                    <div className ="site-informations">
+                        <p>Copyright © 2024 - Tous droits réservés. </p>
+                        <a className="contact" href="/Contact">Contact</a>
+                        <a className="cgv" href="/Cgv">Conditions générales de vente</a>
+                        <a className="FAQ" href="/FAQ">FAQ</a>
+                    </div>
                 </div>
-                <div className ="site-informations">
-                    <p>Copyright © 2024 - Tous droits réservés. </p>
-                    <p>Contact</p>
-                    <p>Conditions générales de vente</p>
-                    <p>FAQ</p>
-                </div>
-            </div>
 
-        </footer>
-    )
-}
+            </footer>
+        )
+    }
 
-export default Footer;
+    export default Footer;
