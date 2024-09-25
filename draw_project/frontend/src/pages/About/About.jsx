8f63f4359ab22ce1aad3debb9aac605c4c419559
Gulbahar57 <158152207+Gulbahar57@users.noreply.github.com>
2024-09-25 15:53:04 +0200

    feeding the about page

--- draw_project/frontend/src/pages/About/About.jsx
+++ draw_project/frontend/src/pages/About/About.jsx
@@ -9,7 +9,12 @@ function About(){
         <div className="about-page">
             <Navbar />
         <main className="about-content">
-            <h1 className="about-text">A propos d'Azekar't...</h1>
+            <div className="about-container">
+            <h1 className="about-title">A propos de moi...</h1>
+            <p className="text-introduction">
+            Je m’appelle G, et je suis une artiste passionnée depuis l’âge de 8 ans. Mon aventure avec le dessin a commencé de façon plutôt inattendue. Tout est parti d’un garçon dans ma classe qui dessinait. Je me souviens encore d'être assise à côté de lui, totalement fascinée par son talent. Il m'a tellement inspirée que j’ai pris un crayon et un carnet pour essayer à mon tour. C’était le tout début d'une longue histoire entre moi et le dessin.
+            </p>
+            </div>
         </main>
             <Footer />
         </div>
